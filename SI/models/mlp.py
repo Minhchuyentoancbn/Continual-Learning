0136d08e86964e6328753b6909e75f73b02cfbfe
Le Duc Minh <minhmeo7a1@gmail.com>
2023-04-01 01:03:34 +0700

    Add models

--- SI/models/mlp.py
+++ SI/models/mlp.py
@@ -52,7 +52,71 @@ class MLP(nn.Module):
         x = self.logits(x)
         return x
     
+class MLPDropout(nn.Module):
+    """Simple MLP."""
+
+    def __init__(self, out_dim=10, in_channel=1, img_size=32, hidden_dim=256):
+        """Initialize MLP.
+
+        Parameters
+        ----------
+        out_dim : int, optional
+            output dimension, by default 10
+
+        in_channel : int, optional
+            input channel, by default 1
+
+        img_size : int, optional
+            image size, by default 32
+
+        hidden_dim : int, optional
+            hidden dimension, by default 256
+
+        Returns
+        -------
+        None
+        """
+        super().__init__()
+        self.in_dim = in_channel * img_size * img_size
+        self.linear = nn.Sequential(
+            nn.Dropout(0.2),
+            nn.Linear(self.in_dim, hidden_dim),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5)
+        )
+        self.last = nn.Linear(hidden_dim, out_dim)
+
+
+    def features(self, x):
+        x = self.linear(x.view(-1, self.in_dim))
+        return x
+    
+
+    def logits(self, x):
+        x = self.last(x)
+        return x
+
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.logits(x)
+        return x
+
+
+def MLPDropout2000_MNIST():
+    """Return MLP with 256 hidden units on MNIST."""
+    return MLPDropout(hidden_dim=2000, img_size=28)
+
+    
 
 def MLP256_MNIST():
     """Return MLP with 256 hidden units on MNIST."""
     return MLP(hidden_dim=256, img_size=28)
+
+
+def MLP2000_MNIST():
+    """Return MLP with 256 hidden units on MNIST."""
+    return MLP(hidden_dim=2000, img_size=28)
